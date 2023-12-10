f64e919e583457c1fd58896873544e1c81605ff9
Kilativ54 <vitalikechkalov@gmail.com>
2023-12-10 12:15:12 +0100

    loader

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+// import car from 'assets/loader.svg';
+
+import { LoadWrapper } from './Loader.styled';
+
+const Loader = () => {
+  return (
+    <LoadWrapper>
+      <span className="loader"></span>
+    </LoadWrapper>
+  );
+};
+
+export default Loader;
