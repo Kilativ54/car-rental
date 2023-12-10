f9775db58911dd8eb2d7eaa1821a31cabf6d8187
Kilativ54 <vitalikechkalov@gmail.com>
2023-12-10 12:06:35 +0100

    finished layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,29 @@
-import { Outlet } from "react-router-dom";
-import Header from "components/Header/Header";
-// import { Suspense } from 'react';
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
 
-function Layout(){
-    return(
-        <div>
-            <Header/>
-            <main><Outlet/></main>
-            
-        </div>
-    )
+import Footer from 'components/Footer/Footer';
+import Loader from 'components/Loader/Loader';
+import Header from 'components/Header/Header';
+import { Container, Main, MainWrapper } from './Layout.styled';
+import ScrollToTop from 'components/ScrollToTop/ScrollToTop';
+
+function Layout() {
+  return (
+    <MainWrapper>
+      <ScrollToTop />
+      {/* <Container> */}
+      <Header />
+      {/* </Container> */}
+      <Main>
+        <Container>
+          <Suspense fallback={<Loader />}>
+            <Outlet />
+          </Suspense>
+        </Container>
+      </Main>
+      <Footer />
+    </MainWrapper>
+  );
 }
 
 export default Layout;
