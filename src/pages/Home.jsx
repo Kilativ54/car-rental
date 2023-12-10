d6783a5904a58444d64fce4ae3e845e4405900d5
Kilativ54 <vitalikechkalov@gmail.com>
2023-12-10 12:18:20 +0100

    fix home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
-
+import HomeDescription from 'components/HomeDescription/HomeDescription';
+import Section from 'components/Section/Section';
+import Title from 'components/Title/Title';
 
 const Home = () => {
-    return(
-        <div><h1>Home</h1></div>
-    )
-}
+  return (
+    <Section>
+      <Title title="Welcome to our Car Rental Service!" />
+      <HomeDescription />
+    </Section>
+  );
+};
 
 export default Home;
