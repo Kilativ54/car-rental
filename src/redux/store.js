b0640bd8afdc13ab35fb822057105f8f88bc0ecb
Kilativ54 <vitalikechkalov@gmail.com>
2023-12-10 12:19:38 +0100

    fix store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,31 +1,31 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import {
-//   persistStore,
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from 'redux-persist';
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
-// import { carsReducer } from './cars/carsSlice';
+import { carsReducer } from './cars/carsSlice';
 
-// import { favoriteReducer } from './favorite/favoriteSlice';
-// import { filterReducer } from './filter/filterSlice';
+import { favoriteReducer } from './favorite/favoriteSlice';
+import { filterReducer } from './filter/filterSlice';
 
-// export const store = configureStore({
-//   reducer: {
-//     cars: carsReduce,
-//     filter: filterReducer,
-//     favorites: favoriteReducer,
-//   },
-//   middleware: getDefaultMiddleware =>
-//     getDefaultMiddleware({
-//       serializableCheck: {
-//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//       },
-//     }),
-// });
+export const store = configureStore({
+  reducer: {
+    cars: carsReducer,
+    filter: filterReducer,
+    favorites: favoriteReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
-// export const persistor = persistStore(store);
+export const persistor = persistStore(store);
