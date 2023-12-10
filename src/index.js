30d9a0e8ba29389d0e541eea4e29779f3aa95ef7
Kilativ54 <vitalikechkalov@gmail.com>
2023-12-10 12:22:07 +0100

    fix index js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-// import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { persistor, store } from 'redux/store';
 import 'modern-normalize/modern-normalize.css';
-// import { PersistGate } from 'redux-persist/integration/react';
-// import {persistor,store } from './redux/store'
+import './styles/loader.css';
+import './styles/variables.css';
+import './styles/fonts.css';
 import './index.css';
+import './styles/scroll.css';
+// import './helpers/scroll';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-  <BrowserRouter basename="/car-rental">
-    <App />
-  </BrowserRouter>
-  </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter basename="/car-rental">
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
