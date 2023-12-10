7ee3a5c1c6ebf5346fa681d5182e4ab542f8221e
Kilativ54 <vitalikechkalov@gmail.com>
2023-12-10 12:17:46 +0100

    fix favorites

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,7 +1,23 @@
+import { useSelector } from 'react-redux';
+
+import CarsList from 'components/CarsList/CarsList';
+import NoCarsSection from 'components/NoCarsSection/NoCarsSection';
+import Title from 'components/Title/Title';
+import { selectFavorite } from 'redux/favorite/favoriteSelectors';
+
 const Favorites = () => {
-    return(
-        <div><h1>Favorite</h1></div>
-    )
-}
+  const { favoriteCars } = useSelector(selectFavorite);
+
+  return (
+    <section>
+      <Title title="Your Favorite Cars" />
+      {favoriteCars.length > 0 ? (
+        <CarsList carsArray={favoriteCars} />
+      ) : (
+        <NoCarsSection />
+      )}
+    </section>
+  );
+};
 
 export default Favorites;
