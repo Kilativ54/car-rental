32b4f3977667ddc7b90d47ce22091f5071ca21b4
Kilativ54 <vitalikechkalov@gmail.com>
2023-12-10 12:06:11 +0100

    finished header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,31 @@
-import Navigation from "components/Navigation/Navigation";
+import { motion } from 'framer-motion';
+import { useMediaQuery } from 'react-responsive';
+
+import carLogo from 'assets/rental-car-logo.png';
+import { HeaderStyles, Logo, LogoImg } from './Header.styled';
+import MobileMenu from 'components/MobileMenu/MobileMenu';
+import Navigation from 'components/Navigation/Navigation';
 
 const Header = () => {
-    return(
-<Navigation/>
-    )
-}
+  const isMobile = useMediaQuery({
+    query: '(max-width: 768px)',
+  });
+
+  return (
+    <HeaderStyles>
+      <motion.div
+        initial={{ opacity: 0, x: -100 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1.15 }}
+        viewport={{ once: true, amount: 0.2 }}
+      >
+        <Logo to="/">
+          <LogoImg src={carLogo} alt="logo" width="200" height="80" />
+        </Logo>
+      </motion.div>
+      {isMobile ? <MobileMenu /> : <Navigation />}
+    </HeaderStyles>
+  );
+};
 
 export default Header;
--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -1,2 +1,25 @@
+import styled from '@emotion/styled';
+import { NavLink } from 'react-router-dom';
 
+export const HeaderStyles = styled.header`
+  box-shadow: 0px 10px 30px -10px var(--separator-color);
+  border-radius: 8px;
+  padding: 20px;
+  margin: 0 auto;
+  width: 100%;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: var(--bg-color);
+  z-index: 999;
+`;
 
+export const Logo = styled(NavLink)`
+  /* position: relative; */
+`;
+
+export const LogoImg = styled.img`
+  width: 100%;
+  max-height: 100%;
+`;
